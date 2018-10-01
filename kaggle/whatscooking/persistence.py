843730141f5d664f42e21fb5777f6fe8245aa8e8
Anil Narayan <anilknarayan@users.noreply.github.com>
2018-10-01 04:48:17 +0000

    Changed persistence location of intermediate dataset

--- kaggle/whatscooking/persistence.py
+++ kaggle/whatscooking/persistence.py
@@ -1,10 +1,10 @@
 # START Functions to persist and retrieve data to save time
 import pickle    
 def save_obj(obj, name ):
-    with open('C:\tempjunk\intermediate\' + name + '.pkl', 'wb') as f:
+    with open(r'C:\tempjunk\intermediate\' + name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name ):
-    with open('C:\tempjunk\intermediate\' + name + '.pkl', 'rb') as f:
+    with open(r'C:\tempjunk\intermediate\' + name + '.pkl', 'rb') as f:
          return pickle.load(f)
 # END Functions to persist and retrieve data to save time
